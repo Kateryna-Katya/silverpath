9f9223a61d1b1908c9787e5760693dd43e8b93dc
Kateryna <katyaukr2014@gmail.com>
2026-06-30 11:52:28 +0300

    add

---
+++ src/js/accordion.js
@@ -0,0 +1,28 @@
+document.querySelectorAll('.faq-acc-el-title').forEach(trigger => {
+    trigger.addEventListener('click', () => {
+        const parentElement = trigger.closest('.faq-acc-el');
+        const panel = parentElement.querySelector('.faq-acc-el-descr-frame');
+        const svg = trigger.querySelector('svg');
+        const isOpen = parentElement.classList.contains('open');
+
+        const plusIcon = `
+<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M24 12C24 12.2652 23.8946 12.5196 23.7071 12.7071C23.5196 12.8946 23.2652 13 23 13H13V23C13 23.2652 12.8946 23.5196 12.7071 23.7071C12.5196 23.8946 12.2652 24 12 24C11.7348 24 11.4804 23.8946 11.2929 23.7071C11.1054 23.5196 11 23.2652 11 23V13H1C0.734784 13 0.48043 12.8946 0.292893 12.7071C0.105357 12.5196 0 12.2652 0 12C0 11.7348 0.105357 11.4804 0.292893 11.2929C0.48043 11.1054 0.734784 11 1 11H11V1C11 0.734784 11.1054 0.48043 11.2929 0.292893C11.4804 0.105357 11.7348 0 12 0C12.2652 0 12.5196 0.105357 12.7071 0.292893C12.8946 0.48043 13 0.734784 13 1V11H23C23.2652 11 23.5196 11.1054 23.7071 11.2929C23.8946 11.4804 24 11.7348 24 12Z" fill="white" />
+</svg>`;
+
+        const minusIcon = `
+<svg width="24" height="2" viewBox="0 0 24 2" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M24 1C24 1.26522 23.8946 1.51957 23.7071 1.70711C23.5196 1.89464 23.2652 2 23 2H1C0.734784 2 0.48043 1.89464 0.292893 1.70711C0.105357 1.51957 0 1.26522 0 1C0 0.734784 0.105357 0.48043 0.292893 0.292893C0.48043 0.105357 0.734784 0 1 0H23C23.2652 0 23.5196 0.105357 23.7071 0.292893C23.8946 0.48043 24 0.734784 24 1Z" fill="white" />
+</svg>`;
+
+        if (!isOpen) {
+            parentElement.classList.add('open');
+            panel.style.maxHeight = panel.scrollHeight + 'px';
+            svg.outerHTML = minusIcon;
+        } else {
+            parentElement.classList.remove('open');
+            panel.style.maxHeight = '0';
+            svg.outerHTML = plusIcon;
+        }
+    });
+});
